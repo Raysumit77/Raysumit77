c84706dc707713c506db01908ac30505e612dc37
sumit yadav <ahsiray79@gmail.com>
2024-10-09 15:01:29 +0530

    literals

---
+++ src/CWHliterals.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+
+public class CWHliterals {
+    private static Object Sytem;
+
+    public static void main(String[] args) {
+        byte age = 23;
+        int age2 = 45;
+        short age3 = 23;
+        long ageDino = 57777777777777L;
+        char ch = 'A';
+        float f1 = 4.5f;
+            double d1 = 3.55;
+            boolean a = true;
+     System.out.println(age);
+     String str = "sumit";
+     System.out.println(str);
+
+
+    }
+}
