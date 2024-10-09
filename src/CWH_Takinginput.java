9b68e7cbba7f7521f2183c8ddbf9f4d0c9de9de2
sumit yadav <ahsiray79@gmail.com>
2024-10-09 15:17:32 +0530

    taking input from the user

---
+++ src/CWH_Takinginput.java
@@ -0,0 +1,25 @@
+import java .util.Scanner;
+
+public class CWH_Takinginput {
+    public static void main(String[] args) {
+        System.out.println("Taking Input From the User");
+        Scanner sc = new Scanner( System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println("The Sum of These number is :");
+        System.out.println(sum);
+
+        System.out.println("Taking Input From the User");
+        Scanner sb = new Scanner( System.in);
+        System.out.println("Enter age 1");
+        int d = sb.nextInt();
+        System.out.println("Enter age  2");
+        int s = sb.nextInt();
+        int together = d + s ;
+        System.out.println("The Sum of These age  is :");
+        System.out.println(together);
+    }
+}
