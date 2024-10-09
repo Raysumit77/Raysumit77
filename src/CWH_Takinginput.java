209a1c114fadad7e66bad408b5969eea84705d5a
sumit yadav <ahsiray79@gmail.com>
2024-10-09 15:52:13 +0530

    taking input from the user with boolean

--- src/CWH_Takinginput.java
+++ src/CWH_Takinginput.java
@@ -4,22 +4,27 @@ public class CWH_Takinginput {
     public static void main(String[] args) {
         System.out.println("Taking Input From the User");
         Scanner sc = new Scanner( System.in);
-        System.out.println("Enter number 1");
-        int a = sc.nextInt();
-        System.out.println("Enter number 2");
-        int b = sc.nextInt();
-        int sum = a + b ;
-        System.out.println("The Sum of These number is :");
-        System.out.println(sum);
-
-        System.out.println("Taking Input From the User");
-        Scanner sb = new Scanner( System.in);
-        System.out.println("Enter age 1");
-        int d = sb.nextInt();
-        System.out.println("Enter age  2");
-        int s = sb.nextInt();
-        int together = d + s ;
-        System.out.println("The Sum of These age  is :");
-        System.out.println(together);
+//        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+//        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+//        int sum = a + b ;
+//        System.out.println("The Sum of These number is :");
+//        System.out.println(sum);
+//
+//        System.out.println("Taking Input From the User");
+//        Scanner sb = new Scanner( System.in);
+//        System.out.println("Enter age 1");
+//        int d = sb.nextInt();
+//        System.out.println("Enter age  2");
+//        int s = sb.nextInt();
+//        int together = d + s ;
+//        System.out.println("The Sum of These age  is :");
+//        System.out.println(together);
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
     }
 }
