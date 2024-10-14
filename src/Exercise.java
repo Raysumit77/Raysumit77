4e4902bae165d5345ba294f983a69c8a973ab21d
sumit yadav <ahsiray79@gmail.com>
2024-10-14 11:21:08 +0530

    asking user have a good day

---
+++ src/Exercise.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercise {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner( System.in);
+//        System.out.println("Enter marks of subject 1");
+//    float subject1 = sc.nextInt();
+//        System.out.println("Enter marks of subject 2");
+//       float subject2 = sc.nextInt();
+//        System.out.println("Enter marks of subject 3");
+//      float subject3 = sc.nextInt();
+//        System.out.println("Enter marks of subject 4");
+//      float subject4 = sc.nextInt();
+//        System.out.println("Enter marks of subject 5");
+//       float subject5 = sc.nextInt();
+//        float percentage = (subject1 + subject2 + subject3 + subject4 + subject5 )/500*100;
+//        System.out.println(percentage);
+
+        Scanner sc = new Scanner( System.in);
+        System.out.println("What is your name");
+        String name = sc.next();
+        System.out.println("Hello " + name + " Have a Good Day!");
+
+    }
+}
+
